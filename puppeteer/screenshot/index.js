b9ade9e701429990b18d7b01ac04a5298fd4072c
Josh Scott <45861892+Josh-Canopi@users.noreply.github.com>
2024-02-22 09:52:51 +1100

    WIP

--- puppeteer/screenshot/index.js
+++ puppeteer/screenshot/index.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 module.exports = async function (context, req) {
     const url = req.query.url || "https://google.com/";
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
     const page = await browser.newPage();
     await page.goto(url);
     const screenshotBuffer = await page.screenshot({ fullPage: true });
